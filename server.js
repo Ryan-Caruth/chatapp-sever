60f68786968b5eac9dc58c56a5b8135ebb4e73e0
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-13 14:55:30 -0600

    Set up socket.io to connect and disconnect

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
-//Need to create a server wrapped inside http.createServer()
-
+const express = require("express");
 const app = express();
+const http = require("http");
+//Need to create a server wrapped inside http.createServer()
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+//Use socketio variable to emit and recieve messages to clients
+const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world, soon this will be a chat app");
+  res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log(`user disconnected with id: ${socket.id}`);
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-
